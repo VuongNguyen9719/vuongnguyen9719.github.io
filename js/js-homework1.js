050fb8c4c81ca2b62c7b89f4d19dc8e18d2708d1
VuongNguyen9719 <50986175+VuongNguyen9719@users.noreply.github.com>
2019-06-17 11:08:16 +0700

    up

---
+++ js/js-homework1.js
@@ -0,0 +1,35 @@
+function sayHello() {
+	document.write ('xin chào các bạn');
+}
+sayHello()
+ 
+
+ function sayHello(name) {
+ 	document.write ('xin chao' + name);
+ }
+sayHello(name)
+
+
+ function sayHello(name) {
+
+ 	document.write ('xin chao\"' + name + '\"');
+ }
+sayHello(name)
+
+
+function tinhTong(a,b) {
+	document.write (a + b);
+}
+tinhTong(a,b)
+
+
+function binhPhuong (a) {
+	document.write ( a * a);
+}
+binhPhuong(a)
+
+function theKy (a) {
+	var b = Math.floor((a/100)+1);
+	document.write ('day la the ki' + b);
+}
+theKy(a)
